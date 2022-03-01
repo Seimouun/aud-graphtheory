d9533266185a852e0cb0273b2e4f0f1bd1e36b1d
Seimouun <simon.reitmann>
2022-03-01 21:25:01 +0100

    pixel height done

--- start.py
+++ start.py
@@ -8,37 +8,48 @@ from dateutil.relativedelta import relativedelta
 image = cv2.imread("chart.png") 
 image_height = image.shape[0]
 image_width = image.shape[1]
-money_steps = [100000, 1000000, 10000000]
 
 pixel_money = []
 
-pixel_since_last_line = 0
-line_count = 0
-money_per_line = []
+def is_line(pixel_color):
+    return pixel_color < 254
+def iterate_pixel_money():
+    pixel_count = 0
+    section = 0
+    sub_section = 8
+    section_amount = [100000,100000,1000000,10000000]
 
-line_start = 95
-line_end = 647
-
-for y in range(line_start, line_end):
-    pixel_height = line_end + line_start - 1 - y;
-    if image[pixel_height, 114][2] < 254:
-        print(pixel_since_last_line)
-        for count in range(0, pixel_since_last_line + 1):
-            last_moneten = 0
-            if y - line_start > 0:
-                print(str(len(pixel_money)) + ", " + str(y));
-                last_moneten = pixel_money[y - line_start -1]
-            pixel_money.append(last_moneten + money_steps[int(line_count / 10)] / pixel_since_last_line);
-        line_count+=1;
-        pixel_since_last_line = 0
-
-    image[pixel_height, 114] = [0,0,255]
-    pixel_since_last_line+=1;
+    line_start = 95
+    line_end = 647
+    for y in range(0, line_end - line_start):
+        height = line_end - y
+        if(is_line(image[height, 114][2])):
+            for ___i in range(0, pixel_count):
+                prev_index_money = 0
+                try:
+                    prev_index_money = pixel_money[pixel_money.__len__()-1]
+                except:
+                    pass
+                pixel_money.append(prev_index_money + section_amount[section] / pixel_count)
+            
+            pixel_count = 0
+            sub_section += 1
+            if(sub_section % 9 == 0):
+                sub_section = 0
+                section += 1
+        else:
+            pixel_count += 1
 
 def get_cash_mula_for_pixel_height(hight_key):
-    return pixel_money[hight_key]
+    return round(pixel_money[hight_key],2)
 
-print("mula fo 76: " + str(get_cash_mula_for_pixel_height(76)))
+iterate_pixel_money()
+
+#example:
+# use get_cash_mula_for_pixel_height(74) returns 100000.0 rounded to 2 decimals
+# money indexes go from 0 to 551
+# height of the pixel relative to the baseline of the graph is the respected money for that pixel
+print("75:" + str(get_cash_mula_for_pixel_height(74)) + "$")
 
 
 def getPixelDate(x):
